7181ac5830a3e708c2099a3f9a67345860190c35
Александр Сминов ПрИ-202 <smirnovalexandr234@gmail.com>
2025-06-08 09:46:49 +0500

    Я забыл залить на гит.

--- stop.sh
+++ stop.sh
@@ -3,6 +3,13 @@
 if [ $# -eq 0 ]
 then
   docker-compose down --rmi local --remove-orphans
+#      down — останавливает и удаляет контейнеры, сети и тома.
+#      --rmi local — удаляет локальные образы, созданные docker-compose build.
+#      --remove-orphans — удаляет контейнеры, не указанные в docker-compose.yml.
 else
   docker-compose rm -fsv $@
+#      rm — удаляет только контейнеры без сетей и томов
+#      -f --force — принудительное удаление даже если контейнеры запущены.
+#      -s --stop — останавливает контейнеры перед удалением.
+#      -v --volumes — удаляет анонимные тома (но не именованные).
 fi
